f7ebf91ab3aa8fa18dfb188de25de3c65e005cfe
Mikhail <misha220993@gmail.com>
2022-04-06 15:43:47 +0400

    init

---
+++ Task Lists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task Lists
+//
+//  Created by Михаил Киржнер on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
